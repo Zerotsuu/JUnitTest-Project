b69dadd5f62a58eb2868004c7bebc12931513fb8
Don Joseph Lachica <44009377+Zerotsuu@users.noreply.github.com>
2024-07-06 10:33:17 +0800

    test commit 2

--- demo1/src/main/java/com/example/shoppingcart/testfile.java
+++ demo1/src/main/java/com/example/shoppingcart/testfile.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-//    testing commit
+//    testing commit number 2
     private List<Product> products;
 
     public ShoppingCart() {
